b21fb66471210823094ecbff57d7c6fe3578c2fa
Ruslan Tendora <ruslantendora@gmail.com>
2022-11-16 17:23:13 +0200

    add 1 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -8,12 +8,14 @@ const galleryItemsEl = galleryItems
   .map(
     ({ preview, original, description }) =>
       `<div class=""gallery__item">
-            <img
-                class="gallery__image"
-                src="${preview}" 
-                alt="${description}" 
-                data-source="${original}">
-        </div>`
+        <a class="gallery__link" href="large-image.jpg">
+          <img
+              class="gallery__image"
+              src="${preview}" 
+              alt="${description}" 
+              data-source="${original}">
+        </a>
+      </div>`
   )
   .join("");
 
@@ -22,30 +24,11 @@ galleryBoxEl.insertAdjacentHTML("afterbegin", galleryItemsEl);
 galleryBoxEl.addEventListener("click", onAddImageClick);
 
 function onAddImageClick(event) {
-  console.log(event);
-  console.log(event.target.nodeName);
+  event.preventDefault();
+
   if (event.target.nodeName !== "IMG") {
     return;
   }
-  console.log(event.target.dataset.original);
-  const instance = basicLightbox.create(`
-    <div class="modal">
-        <img src="${event.target.dataset.sourse}">
-    </div>
-`);
 
-  instance.show();
-}
-
-{
-  /* <div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
-    <img
-      class="gallery__image"
-      src="small-image.jpg"
-      data-source="large-image.jpg"
-      alt="Image description"
-    />
-  </a>
-</div>;  */
+  basicLightbox.create(`<img src="${event.target.dataset.source}">`).show();
 }
