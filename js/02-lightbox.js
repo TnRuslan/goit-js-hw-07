1f7801efc054b554f54f55592817c2288a8784a4
Ruslan Tendora <ruslantendora@gmail.com>
2022-11-16 22:51:10 +0200

    add secon gallery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+{
+  /* <a class="gallery__item" href="large-image.jpg">
+  <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+</a>; */
+}
+
+const galleryBoxEl = document.querySelector(".gallery");
+
+const galleryItemsEl = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href="${original}">
+          <img
+              class="gallery__image"
+              src="${preview}" 
+              alt="${description}" />
+        </a>`
+  )
+  .join("");
+
+galleryBoxEl.insertAdjacentHTML("afterbegin", galleryItemsEl);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
